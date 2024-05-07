7dc4a74b71c66f4d20ec339dbc2af66bba836a8a
EdoardoSquillaci <edoardosquillaci@gmail.com>
2024-05-08 01:59:11 +0200

    Esercizio Quasi Completo

--- train.js
+++ train.js
@@ -1,12 +1,31 @@
 'use strict'
 
 
-const kmDaPercorrere =  parseInt(Number(prompt ('Inserisci i km da percorrere...'))); 
+
+const kmDaPercorrere = (Number(prompt ('Inserisci i km da percorrere...'))); 
 
 const etaUtente = parseInt(Number(prompt ('Inserisci la tua età...')));
 
+let prezzoBiglietto = kmDaPercorrere * 0.21;
 
-const prezzoBiglietto = kmDaPercorrere * 0.21;
+const scontoMinori = (prezzoBiglietto * 20) / 100
+
+const scontoSenior = (prezzoBiglietto * 40) / 100
+
+
+if(etaUtente<18){
+    prezzoBiglietto = prezzoBiglietto - scontoMinori
+    console.log (prezzoBiglietto)
+}
+
+else if (etaUtente>65){
+    prezzoBiglietto = prezzoBiglietto - scontoSenior
+    console.log (prezzoBiglietto)
+}
+
+else{
+    console.log (prezzoBiglietto)
+}
 
 
 document.getElementById ('km').innerHTML = kmDaPercorrere
@@ -14,9 +33,6 @@ document.getElementById ('km').innerHTML = kmDaPercorrere
 document.getElementById ('eta').innerHTML = etaUtente
 
 document.getElementById ('prezzo-biglietto').innerHTML = prezzoBiglietto
-
-console.log (prezzoBiglietto)
-
 
 
 
